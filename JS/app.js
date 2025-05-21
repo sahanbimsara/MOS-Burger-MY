a1d48f27beb8cf83a57f9281731aaef7f3e06ca6
sahanbimsara <sahanbimsara2060@gmail.com>
2025-05-21 08:02:29 +0530

    javaScript

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,192 @@
- 
+const burgers = [
+    {
+        title: "Classic Burger (Large)",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhCrEENCElGIIXTS1cmsf-zAEU_m6V-RK8nQ&s",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    },
+    {
+        title: "Classic Burger (Large)",
+        img: "../img/ClassicLarge.jpg",
+        desc: "Large Burger"
+    }
+];
+
+const menu = document.getElementById("homeCards");
+
+burgers.forEach(burger => {
+    menu.innerHTML += `
+    <div class="col-md-3">
+        <div class="card mb-4" style="width: 100%;">
+            <img src="${burger.img}" style="height:150px; object-fit: cover;" alt="${burger.title}">
+            <div class="card-body">
+                <h5 class="card-title">${burger.title}</h5>
+                <p class="card-text">${burger.desc}</p>
+                <a href="orderManage.html" class="btn btn-primary">
+                <i class="bi bi-cart me-2"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+    `;
+});
